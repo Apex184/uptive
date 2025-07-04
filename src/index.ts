2a0af473204fd555eccb0bcdae87b37d43df0705
Akindele Simeon <adisaakindele2@gmail.com>
2025-07-04 17:54:10 +0100

    handle cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+const allowedOrigins = ['http://localhost:5173', 'https://your-production-frontend.com'];
+app.use(cors({
+    origin: allowedOrigins
+}));
 app.use(express.json());
 
 app.use('/auth', authRouter);
